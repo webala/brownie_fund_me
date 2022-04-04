d9c31d7eec38084fa4817a23e1c33382944fe674
webala <webala1001@gmail.com>
2022-04-04 09:01:48 +0300

    can deploy to persistant ganache chain

--- fund_me/scripts/deploy.py
+++ fund_me/scripts/deploy.py
@@ -1,28 +1,30 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_DEVELOPMENT_ENVIRONMENTS,
+)
 from web3 import Web3
+
 
 def deploy_fund_me():
     account = get_account()
 
-    if not network.show_active() == "development":
+    if not network.show_active() in LOCAL_DEVELOPMENT_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]  # price feed address for persistent network(rinkeby)
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying Mocks ...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, Web3.toWei(2000, 'ether'), {"from": account}
-        )
-        print("Mocks deployed")
-        price_feed_address = mock_aggregator.address
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address  # Use latest MockV3Aggregator
 
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": account},  # pass price feed to fund me constructor
-        publish_source=config["networks"][network.show_active()].get("verify"), # source code will be published
-    )  
+        publish_source=config["networks"][network.show_active()].get(
+            "verify"
+        ),  # source code will be published
+    )
     print(f"Fund me deployed at {fund_me.address}")
 
 
--- fund_me/scripts/helpful_scripts.py
+++ fund_me/scripts/helpful_scripts.py
@@ -1,7 +1,25 @@
-from brownie import accounts, config, network
+from brownie import accounts, config, network, MockV3Aggregator
+from web3 import Web3
+
+
+DECIMALS = 18
+STARTING_PRICE = 2000
+LOCAL_DEVELOPMENT_ENVIRONMENTS =['development', 'ganache-local']
+
 
 def get_account():
-    if network.show_active() == 'development':
+    if network.show_active() in LOCAL_DEVELOPMENT_ENVIRONMENTS:
         return accounts[0]
     else:
-        return accounts.add(config['wallets']['from_key'])
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks ...")
+    if len(MockV3Aggregator) <= 0:
+        # check if there is a MockV3Aggregator deployed in the same network
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+        )
+        print("Mocks deployed")
